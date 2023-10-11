9b71353938e542d84e82f3bb10a3ac9e85ba5391
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-11 08:05:05 +0100

    Second Commit: Implement skeleton index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const PORT = 3030;
+const app = express();
+const todoRoutes = require("./routes/todoRoutes");
+
+
+app.use("/todo/", todoRoutes);
+app.listen(PORT, () => {
+    console.log("The server is listening on port " + PORT);
+});
