7abfb019b876f4da109e65bbb4bdedc31f4fa785
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-12 09:54:09 +0100

    Fifth Commit: Implement connection to mongodb database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = 3030;
 const app = express();
 const todoRoutes = require("./routes/todoRoutes");
 
+mongoose.connect("mongodb://localhost/todolist")
+    .then(() => console.log("Connected successfully!"))
+    .catch((err) => console.error(err));
 
 app.use("/todo/", todoRoutes);
 app.listen(PORT, () => {
