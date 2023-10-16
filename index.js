d78380f42bc372e3a174df11d12c717ccf250c01
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-16 09:26:14 +0100

    Seventeen Commit: Fix Axios Network Error - "blocked by CORS policy"

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const PORT = 3030;
 const app = express();
 const todoRoutes = require("./routes/todoRoutes");
 
 app.use(express.json());
+app.use(cors());
+
 mongoose.set("strictQuery", true);
 
 mongoose.connect("mongodb://0.0.0.0/todolist")
