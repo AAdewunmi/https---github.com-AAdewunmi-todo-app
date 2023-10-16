559d10ceda6d6536f16c447636d8c73e7c745df0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-10-16 06:54:46 +0100

    Fifteen Commit: Implement PUT request handler

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,9 +2,6 @@ const router = require("express").Router();
 const Todo = require("../models/Todo");
 
 router.get("/", (req, res) => {
-    /* Todo.find().then((result) => {
-        console.log(result);
-    }); */
     Todo.find((err, result) => {
       if(err) throw new Error(err);
       console.log(result);
@@ -12,21 +9,20 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    /* Todo.create().then(req.body, (result) => {
-      res.send({result});
-      console.log(result);
-    }); */
     Todo.create(req.body, (err, result) => {
       if (err) throw new Error(err);
       console.log(result);
     });
 });
 
+router.put("/:id", (req, res) => {
+  Todo.findOneAndUpdate({ _id: req.params.id }, req.body, (err, result) => {
+    if (err) throw new Error(err);
+    console.log(result);
+  });
+});
+
 router.delete("/:id", (req, res) => {
-  /* Todo.create().then(req.body, (result) => {
-      res.send({result});
-      console.log(result);
-    }); */
   Todo.findOneAndRemove({_id: req.params.id}, (err, result) => {
     if (err) throw new Error(err);
     console.log(result);
